0a4308bdfb4404af872b29ecb8b6a71b094f4334
Nishith <nishithza@gmail.com>
2020-07-06 16:07:27 +0530

    added homepage ,here all the articles will be displayed with a small discription ,the end user has the option to go view the full blog

--- backend/routes/userRoutes/homepage.js
+++ backend/routes/userRoutes/homepage.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const Article= require('./../../models/articleModel');
 
 router.get('/', async(req,res)=>{
-    let articles=[{
-        id:00,
-        title:'nis',
-        description:'11',
-        body:'23',
-    }]
-    //const articles=await Article.find().sort({ createdAt :'desc'});
+    // let articles=[{
+    //     id:00,
+    //     title:'nis',
+    //     description:'11',
+    //     body:'23',
+    // }]
+    const articles=await Article.find().sort({ createdAt :'desc'});
     res.render('./../../frontend/homepage.ejs',{
         articles:articles,
     });
