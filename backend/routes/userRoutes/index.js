4b54f2b524c7a6537b3cc1dfa8020e0fa9423a7b
sushant <sushant871998>
2020-07-07 03:59:00 +0530

    Updated the index.js under userRoutes to point to correct route

--- backend/routes/userRoutes/index.js
+++ backend/routes/userRoutes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('Welcome');
+    const articles=[{
+        title:"My first Article",
+        description:"This is a test Article",
+        body:"Test article-1",
+        createdAt:new Date()
+    }]  
+    res.render('index',{text:articles})
 });
 module.exports = router;
+
