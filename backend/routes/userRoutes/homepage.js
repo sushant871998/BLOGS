fbceff41f2890a951dd5e3738d2369a69cd12ab0
Nishith <nishithza@gmail.com>
2020-07-06 15:31:36 +0530

    Setting up database connection using mongoose ,created homepage route and its related front end files using ejs and bootstrap

---
+++ backend/routes/userRoutes/homepage.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Article= require('./../../models/articleModel');
+
+router.get('/', async(req,res)=>{
+    let articles=[{
+        id:00,
+        title:'nis',
+        description:'11',
+        body:'23',
+    }]
+    //const articles=await Article.find().sort({ createdAt :'desc'});
+    res.render('./../../frontend/homepage.ejs',{
+        articles:articles,
+    });
+});
+module.exports = router;
