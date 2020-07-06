fab79a061210dd3a317c403beabfaa9ac3e7d813
Nishith <nishithza@gmail.com>
2020-07-06 15:33:17 +0530

    Connection to mongo db atlas

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,19 @@
 const express= require('express');
 const app=express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-
+//Database connection
+require('dotenv').config();
+const databaseUri=process.env.URI;
+mongoose.connect(databaseUri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true})
+        .then(res=>{
+            console.log('Database has been connected');
+        })
+        .catch(err=>{
+            console.log('Database connection error');
+        });
+        
 //Middleware
 app.set('view engine','ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/',require('./routes/userRoutes/index'));
 app.use('/login',require('./routes/userRoutes/userLogin'));
 app.use('/register',require('./routes/userRoutes/userRegister'));
-
+app.use('/homepage',require('./routes/userRoutes/homepage'))
 
 app.listen(PORT,console.log(`Server started on port ${PORT}`));
