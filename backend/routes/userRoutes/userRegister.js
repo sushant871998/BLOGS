523200687d46d3246a4c0822517b139a104266eb
Nishith <nishithza@gmail.com>
2020-07-07 10:11:12 +0530

    Added Register Route ,bcrypt installed for hashing of password

--- backend/routes/userRoutes/userRegister.js
+++ backend/routes/userRoutes/userRegister.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User=require('./../../models/userModel');
 
 router.get('/',(req,res)=>{
-    res.render('register.ejs');
+    res.render('./../../frontend/register.ejs');
+});
+
+router.post('/',(req,res)=>{
+    const{ name ,email, password}=req.body;
+    User.findOne({email : email})
+        .then(user=>{
+            if(user){
+                //User exists
+                res.render('register.ejs');
+            } else {
+                const newUser = new User({
+                    userId:Date.now().toString(),
+                    name:name,
+                    email:email,
+                    password:password
+                });
+                
+                //Hash Password
+                bcrypt.genSalt(10,(err,salt) => 
+                 bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                    if(err) throw err;
+
+                    //Set password to hashed
+                    newUser.password=hash;
+
+                    //Save user
+                    newUser.save()
+                        .then(user=>{
+                            //req.flash('Success_msg' ,'You are now registered and can log in');
+                            res.redirect('/login');
+                        })
+                        .catch(err=>{
+                            console.log('Error while saving '+err);
+                        })
+                }))
+
+            }
+        })
 });
 module.exports = router;
