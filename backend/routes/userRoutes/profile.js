9c72fec214ee71630fac512e2801996cf028a862
sushant871998 <sushanthope@gmail.com>
2020-07-08 05:45:15 +0530

    Added Delete Functionality and fixed bugs

--- backend/routes/userRoutes/profile.js
+++ backend/routes/userRoutes/profile.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('./../../config/auth');
 const Article=require('./../../models/articleModel');
-let userId=0;
 
-router.get('/myArticles', (req,res)=>{
-    res.send('hello');
-    userId=req.user.id;
+
+router.get('/myArticles',ensureAuthenticated,   async(req,res)=>{
+    const articles=await Article.find({userId:req.user.id}).sort({ createdAt :'desc'});
+    res.render('./../../frontend/profile/myArticles.ejs',{
+    articles:articles,
+    name:req.user.name,
+    });
 });
 
-router.get('/myComments', (req,res)=>{
-    res.send('hello');
-    userId=req.user.id;
+router.get('/myComments', ensureAuthenticated, (req,res)=>{
+    res.render('./../../frontend/profile/myComments.ejs');
 });
+
+router.get('/myArticles/:slug', ensureAuthenticated,async(req,res)=>{
+    const article =await Article.findOne({slug:req.params.slug});
+    if(article == null) res.redirect('/myArticles');
+    res.render('./../../frontend/showArticle.ejs',{ article: article });
+});
+
+router.delete('/myArticles/:slug',ensureAuthenticated,async(req,res)=>{
+    const article =await Article.findOne({slug:req.params.slug});
+    if(article==null) res.redirect('/profile/myArticles')
+    await Article.findByIdAndDelete(article.id)
+    res.redirect('/profile/myArticles')
+})
 
 module.exports = router;
