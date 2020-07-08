dc74874b571ce2377a9b5f53e38120495c343d53
Nishith <nishithza@gmail.com>
2020-07-08 17:56:45 +0530

    Multiple Test cases- Login POST route

--- backend/test/appTest.js
+++ backend/test/appTest.js
@@ -2,6 +2,7 @@ const mongooose = require('mongoose');
 const chai = require('chai')
 const chaiHttp= require('chai-http');
 const server = require('./../app');
+const request = require('supertest');
 let expect= chai.expect;
 
 chai.use(chaiHttp);
@@ -49,3 +50,76 @@ describe('Register Page test',()=>{
             })
     });
 });
+
+describe('Login POST route testing',()=>{
+    it('Correct Login values',(done)=>{
+        request(server).post('/login')
+                        .type('form')
+                        .send({
+                            email: 'n@n',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/homepage');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('Invalid Login values',(done)=>{
+        request(server).post('/login')
+                        .type('form')
+                        .send({
+                            email: 'n@n',
+                            password:'1'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/login/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('No Login email  ',(done)=>{
+        request(server).post('/login')
+                        .type('form')
+                        .send({
+                            email: '',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/login/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('No Login password value',(done)=>{
+        request(server).post('/login')
+                        .type('form')
+                        .send({
+                            email: 'n@n',
+                            password:''
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/login/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+    
+    it('Invalid Login values-2',(done)=>{
+        request(server).post('/login')
+                        .type('form')
+                        .send({
+                            email: 'b@b',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/login/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+})
+
