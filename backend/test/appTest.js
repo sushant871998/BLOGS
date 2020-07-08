d142ad0b330436a0fe491e42a8bdfa99d7b3e0c2
Nishith <nishithza@gmail.com>
2020-07-08 18:52:49 +0530

    Test cases -Admin login and register

--- backend/test/appTest.js
+++ backend/test/appTest.js
@@ -107,7 +107,7 @@ describe('Login POST route testing',()=>{
                         })
                         .catch(err=>done(err));
     });
-    
+
     it('Invalid Login values-2',(done)=>{
         request(server).post('/login')
                         .type('form')
@@ -121,5 +121,80 @@ describe('Login POST route testing',()=>{
                         })
                         .catch(err=>done(err));
     });
-})
+});
 
+describe('Register POST route testing',()=>{
+    it('User already exists',(done)=>{
+        request(server).post('/register')
+                        .type('form')
+                        .send({
+                            name: 'Nis',
+                            email: 'n@n',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/register/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('No password field ',(done)=>{
+        request(server).post('/register')
+                        .type('form')
+                        .send({
+                            name: 'Nis',
+                            email: 'n@n',
+                            password:''
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/register/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+});
+
+describe('Admin Login POST route testing',()=>{
+    it('Correct credentials',(done)=>{
+        request(server).post('/admin')
+                        .type('form')
+                        .send({
+                            email: 'n@n',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/admin/home');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('Wrong credentials',(done)=>{
+        request(server).post('/admin')
+                        .type('form')
+                        .send({
+                            email: 'n@nnn',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/admin/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+
+    it('Email not entered',(done)=>{
+        request(server).post('/admin')
+                        .type('form')
+                        .send({
+                            email: '',
+                            password:'1234'
+                        })
+                        .then((res)=>{
+                            expect(res).redirectTo('/admin/fail');
+                            done();
+                        })
+                        .catch(err=>done(err));
+    });
+});
