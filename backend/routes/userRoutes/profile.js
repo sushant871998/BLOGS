3eb4b8bfb216aae3dbbf010c02e4947a02ed4cc9
sushant871998 <sushanthope@gmail.com>
2020-07-08 07:18:25 +0530

    First version of edit

--- backend/routes/userRoutes/profile.js
+++ backend/routes/userRoutes/profile.js
@@ -12,10 +12,10 @@ router.get('/myArticles',ensureAuthenticated,   async(req,res)=>{
     });
 });
 
-router.get('/myComments', ensureAuthenticated, (req,res)=>{
-    res.render('./../../frontend/profile/myComments.ejs');
-});
-
+router.put('/myArticles/edit/:slug', async (req, res, next) => {
+    req.article = await Article.findOne({slug:req.params.slug})
+    next()
+  }, saveArticleAndRedirect('edit'))
 router.get('/myArticles/:slug', ensureAuthenticated,async(req,res)=>{
     const article =await Article.findOne({slug:req.params.slug});
     if(article == null) res.redirect('/myArticles');
@@ -28,5 +28,26 @@ router.delete('/myArticles/:slug',ensureAuthenticated,async(req,res)=>{
     await Article.findByIdAndDelete(article.id)
     res.redirect('/profile/myArticles')
 })
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+  }, saveArticleAndRedirect('new'))
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+      let article = req.article
+      article.title = req.body.title
+      article.description = req.body.description
+      article.body = req.body.body
+      try {
+        article = await article.save()
+        res.redirect(`./../../frontend/showArticle.ejs`,{ article: article })
+      } catch (e) {
+        res.render(`./../../frontend/showArticle.ejs`,{ article: article })
+      }
+    }
+  }
 
+router.get('/myComments', ensureAuthenticated, (req,res)=>{
+    res.render('./../../frontend/profile/myComments.ejs');
+});
 module.exports = router;
