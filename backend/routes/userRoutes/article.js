d5683fffd989a388ea0f2bc27a7ee7d69d93fa5e
Nishith <nishithza@gmail.com>
2020-07-09 15:49:02 +0530

    Setting up multer and gridfs for image uplaod ,article route updated to enable image uplaod

--- backend/routes/userRoutes/article.js
+++ backend/routes/userRoutes/article.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Article=require('./../../models/articleModel');
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require('mongoose');
+const GridFsStorage = require("multer-gridfs-storage");
 const { ensureAuthenticated } = require('./../../config/auth');
 let userId=0;
 
@@ -8,20 +13,58 @@ router.get('/new',ensureAuthenticated, (req,res)=>{
     res.render('./../../frontend/newArticle.ejs');
     userId=req.user.id;
 });
+require('dotenv').config();
+
+const mongoURI=process.env.URI;
+const conn = mongoose.connection
+let gfs;
+conn.once("open", () => {
+    // init stream
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+      bucketName: "uploads"
+    });
+});
+
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString("hex") + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: "uploads"
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+});
+
+const upload = multer({
+    storage
+});
+
 
 router.get('/:slug', ensureAuthenticated, async (req,res)=>{
     const article =await Article.findOne({slug:req.params.slug});
+    const filename=article.imageId;
+    console.log(filename);
     if(article == null) res.redirect('/homepage');
     res.render('./../../frontend/showArticle.ejs',{ article: article });
 });
 
-router.post('/new', ensureAuthenticated, async (req,res)=>{
+router.post('/new', upload.single("file"), async (req,res)=>{
+    console.log(req.file.filename);
     let article = new Article({
         title: req.body.title,
         description: req.body.description,
         body: req.body.body,
         userId:req.user.id,
-
+        imageId:req.file.filename
     });
     try{
         let art=await article.save();
